2b59d04c16c71ed27ca35b46490f1bb72491f2bf
Theo Bonnard <theo.bonnard@cybelangel.com>
2021-11-14 11:12:10 +0100

    refacto(domino): switch active to pickable

--- src/main.rs
+++ src/main.rs
@@ -120,12 +120,101 @@ const MAX_ROUND: usize = 1;
 struct Domino {
     label: u8,
     value: u8,
-    active: bool,
+    pickable: bool,
 }
 
 impl fmt::Debug for Domino {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.label)
+    }
+}
+
+impl Domino {
+    pub fn from(label: u8) -> Domino {
+        match label {
+            // find shorter way to do this
+            21 => Domino {
+                label: 21,
+                value: 1,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            22 => Domino {
+                label: 22,
+                value: 1,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            23 => Domino {
+                label: 23,
+                value: 1,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            24 => Domino {
+                label: 24,
+                value: 1,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            25 => Domino {
+                label: 25,
+                value: 2,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            26 => Domino {
+                label: 26,
+                value: 2,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            27 => Domino {
+                label: 27,
+                value: 2,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            28 => Domino {
+                label: 28,
+                value: 2,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            29 => Domino {
+                label: 29,
+                value: 3,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            30 => Domino {
+                label: 30,
+                value: 3,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            31 => Domino {
+                label: 31,
+                value: 3,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            32 => Domino {
+                label: 32,
+                value: 3,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            33 => Domino {
+                label: 33,
+                value: 4,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            34 => Domino {
+                label: 34,
+                value: 4,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            35 => Domino {
+                label: 35,
+                value: 4,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            36 => Domino {
+                label: 36,
+                value: 4,
+                pickable: true, // not relevant for a "Player Domino". Need refacto Domino handling
+            },
+            _ => panic!("Wrong label"),
+        }
     }
 }
 
@@ -179,82 +268,82 @@ const DOMINOS: [Domino; 16] = [
     Domino {
         label: 21,
         value: 1,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 22,
         value: 1,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 23,
         value: 1,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 24,
         value: 1,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 25,
         value: 2,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 26,
         value: 2,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 27,
         value: 2,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 28,
         value: 2,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 29,
         value: 3,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 30,
         value: 3,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 31,
         value: 3,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 32,
         value: 3,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 33,
         value: 4,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 34,
         value: 4,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 35,
         value: 4,
-        active: true,
+        pickable: true,
     },
     Domino {
         label: 36,
         value: 4,
-        active: true,
+        pickable: true,
     },
 ];
 
